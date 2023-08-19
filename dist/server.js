effb5b093074caecab57c6182ac4dc7042fa6b56
anujsinghwd <anuj_singh@outlook.in>
2023-08-19 13:13:42 +0530

    added search api

--- dist/server.js
+++ dist/server.js
@@ -5,6 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const fs_1 = __importDefault(require("fs"));
+const csv_parser_1 = __importDefault(require("csv-parser"));
 class Server {
     constructor() {
         this.app = (0, express_1.default)();
@@ -19,6 +21,21 @@ class Server {
     routes() {
         this.app.get("/", (req, res) => {
             res.send("Hello, Express!");
+        });
+        this.app.get("/search", (req, res) => {
+            var _a;
+            const searchText = ((_a = req.query.text) === null || _a === void 0 ? void 0 : _a.toString()) || "";
+            const results = [];
+            fs_1.default.createReadStream("./data/cities.csv")
+                .pipe((0, csv_parser_1.default)())
+                .on("data", (row) => {
+                if (row.city.toLowerCase().includes(searchText.toLowerCase())) {
+                    results.push(row);
+                }
+            })
+                .on("end", () => {
+                res.json(results);
+            });
         });
     }
     start() {
