716319124ac1fbb9faf9ee8bf5200120f3494782
anujsinghwd <anuj_singh@outlook.in>
2023-08-19 13:13:25 +0530

    added search api

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express, { Express, Request, Response } from "express";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import fs from "fs";
+import csvParser from "csv-parser";
 
 class Server {
   private app: Express;
@@ -19,6 +21,22 @@ class Server {
   private routes(): void {
     this.app.get("/", (req: Request, res: Response) => {
       res.send("Hello, Express!");
+    });
+
+    this.app.get("/search", (req: Request, res: Response) => {
+      const searchText = req.query.text?.toString() || "";
+
+      const results: any[] = [];
+      fs.createReadStream("./data/cities.csv")
+        .pipe(csvParser())
+        .on("data", (row) => {
+          if (row.city.toLowerCase().includes(searchText.toLowerCase())) {
+            results.push(row);
+          }
+        })
+        .on("end", () => {
+          res.json(results);
+        });
     });
   }
 
